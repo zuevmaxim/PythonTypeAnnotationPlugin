6fa2da1d2d80ef1560d4220d2934cc0f3e48723e
maxim <max-zu99@mail.ru>
2020-09-16 20:38:43 +0300

    Small fixes

--- src/main/kotlin/AddTypeAnnotationAction.kt
+++ src/main/kotlin/AddTypeAnnotationAction.kt
@@ -2,8 +2,6 @@ import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.actionSystem.CommonDataKeys
 import com.intellij.openapi.command.WriteCommandAction
-import com.intellij.psi.PsiComment
-import com.intellij.psi.PsiRecursiveElementWalkingVisitor
 import com.intellij.refactoring.suggested.startOffset
 
 class AddTypeAnnotationAction : AnAction() {
@@ -15,7 +13,6 @@ class AddTypeAnnotationAction : AnAction() {
         val document = editor.document
         val offset = editor.caretModel.currentCaret.offset
         val element = file.findElementAt(offset) ?: return
-
 
         WriteCommandAction.runWriteCommandAction(project) {
             document.insertString(element.startOffset + element.textLength, ": int")
